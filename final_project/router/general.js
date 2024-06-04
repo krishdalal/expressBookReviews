1ce3ca9186009b28d9b3e5cd8e590acc6bbe3a33
krishdalal <84043636+krishdalal@users.noreply.github.com>
2024-06-04 18:44:59 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,40 +4,108 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
+// TASK 6: Registering a new user
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    try{
+        const username = req.body.username;
+        const password = req.body.password;
+        if (username && password) {
+          if (!isValid(username)) {
+            users.push({"username":username,"password":password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+          } else {
+            return res.status(404).json({message: "User already exists!"});
+          }
+        }
+        return res.status(404).json({message: "Unable to register user."});
+    }
+    catch(error){
+        res.status(500).send("Error registering");
+    }
 });
 
-// Get the book list available in the shop
+// TASK 1: Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    try{
+        res.send(JSON.stringify(books, null, 3));
+    }
+    catch(error){
+        res.status(500).send("Error getting book list");
+    }
 });
 
-// Get book details based on ISBN
+// TASK 2: Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    try{
+        const isbn = req.params.isbn;
+        let book = books[isbn];
+    
+        if(book){
+            res.json(book);
+        }
+        else{
+            res.status(404).send(`Unable to find book with ISBN: ${isbn}`);
+        }
+    }
+    catch(error){
+        res.status(500).send("Error getting book details based on ISBN");
+    }
  });
   
-// Get book details based on author
+// TASK 3: Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    try{
+        const author = req.params.author;
+        for (let key in books) {
+            if (books.hasOwnProperty(key)) {
+                let book = books[key];
+                if(book.author==author){
+                    res.send(book);
+                }
+            }
+            res.status(404).send(`Unable to find book of author: ${author}`)
+        }
+    }
+    catch(error){
+        res.status(500).send("Error getting book details based on author");
+    }
 });
 
-// Get all books based on title
+// TASK 4: Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    try{
+        const title = req.params.title;
+        for (let key in books) {
+            if (books.hasOwnProperty(key)) {
+                let book = books[key];
+                if(book.title==title){
+                    res.send(book);
+                }
+            }
+            res.status(404).send(`Unable to find book with title: ${title}`)
+        }
+    }
+    catch(error){
+        res.status(500).send("Error getting book details based on title");
+    }
 });
 
-//  Get book review
+//  TASK 5: Get book reviews
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    try{
+        const isbn = req.params.isbn;
+        let book = books[isbn];
+    
+        if(book){
+            res.send(book.reviews);
+        }
+        else{
+            res.send(`Unable to find book with ISBN: ${isbn}`);
+        }
+    }
+    catch(error){
+        res.status(500).send("Error getting book reviews");
+    }
 });
 
 module.exports.general = public_users;
