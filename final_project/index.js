cefd9616cbc948e47c45a8954f95bedc160a670f
krishdalal <84043636+krishdalal@users.noreply.github.com>
2024-06-04 18:44:09 +0530

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,25 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+    try{
+        if(req.session.authorization) {
+            token = req.session.authorization['accessToken'];
+            jwt.verify(token, "access",(err,user)=>{
+                if(!err){
+                    req.user = user;
+                    next();
+                }
+                else{
+                    return res.status(403).json({message: "User not authenticated"})
+                }
+            });
+        } else {
+            return res.status(403).json({message: "User not logged in"})
+        }
+    }
+    catch(error){
+        res.status(500).send("Unexpected error");
+    }
 });
  
 const PORT =5000;
